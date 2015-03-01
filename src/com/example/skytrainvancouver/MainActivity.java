d21a50e5e8e677e5aee2483fafac9eb474532468
Pablo <Pablo@Pablo_Notebook>
2015-02-28 17:07:27 -0800

    Splash screen  has been created 	- w/ image, title, comment (temporary) 	-w/ task using timer to laod Main Activity An empty main Activity has been created

--- src/com/example/skytrainvancouver/MainActivity.java
+++ src/com/example/skytrainvancouver/MainActivity.java
@@ -2,14 +2,14 @@ package com.example.skytrainvancouver;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
 	}
 }
---
+++ src/com/example/skytrainvancouver/SplashActivity.java
@@ -0,0 +1,32 @@
+package com.example.skytrainvancouver;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SplashActivity extends Activity {
+	static int TIME_TASK = 3000; // each 1000 == 1s
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		
+        TimerTask task = new TimerTask() {
+			
+			@Override
+			public void run() {
+				finish();
+				startActivity(new Intent(SplashActivity.this, MainActivity.class ));
+				
+			}
+		};
+		Timer opening = new Timer();
+		opening.schedule(task, TIME_TASK);
+	}
+}

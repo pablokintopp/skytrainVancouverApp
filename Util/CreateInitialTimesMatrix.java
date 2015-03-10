b306ca56a5774747ec45aca12d64d7876b1b9a73
Pablo <Pablo@Pablo_Notebook>
2015-03-10 12:58:10 -0700

    Evergreen's stations have been added to the matrix and constants

--- Util/CreateInitialTimesMatrix.java
+++ Util/CreateInitialTimesMatrix.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 public class CreateInitialTimesMatrix{
 public static int INFINITE =99999;
-public static int TIMES_MATRIX[][] = new int[47][47];
-public static int PATH_MATRIX[][] = new int[47][47];
+public static int  NUM_STATIONS= 52;
+public static int TIMES_MATRIX[][] = new int[NUM_STATIONS][NUM_STATIONS];
+public static int PATH_MATRIX[][] = new int[NUM_STATIONS][NUM_STATIONS];
 
    public static void main(String args[]){
       Scanner in = new Scanner(System.in);
       
       // starting all distances/times as INFINITE or 0(if is betw itself)
-      for(int i = 0 ; i< 47 ; i++){
-         for(int j = 0 ; j< 47 ; j++){
+      for(int i = 0 ; i< NUM_STATIONS ; i++){
+         for(int j = 0 ; j< NUM_STATIONS ; j++){
             if(i==j){ // in the matrix this means it is a station comparing with itself so the time is 0 and next station is itself
                TIMES_MATRIX[i][j] = 0;
                PATH_MATRIX[i][j] = i;
@@ -39,9 +40,9 @@ public static int PATH_MATRIX[][] = new int[47][47];
          
       }while(line>=0);
     //finding the shortest times betw. every edge/station using floyd warshall implementation 
-     for(int k = 0 ; k <47 ; k++){
-        for(int i = 0; i <47 ; i++){
-            for(int j = 0; j <47 ; j++){
+     for(int k = 0 ; k <NUM_STATIONS ; k++){
+        for(int i = 0; i <NUM_STATIONS ; i++){
+            for(int j = 0; j <NUM_STATIONS ; j++){
                if( TIMES_MATRIX[i][k] + TIMES_MATRIX[k][j] < TIMES_MATRIX[i][j]){
                   
                   TIMES_MATRIX[i][j] = TIMES_MATRIX[i][k] + TIMES_MATRIX[k][j];
@@ -56,16 +57,16 @@ public static int PATH_MATRIX[][] = new int[47][47];
       
     //the output to be used in the floyd marshal algorithm
     System.out.println("TIMES_MATRIX");
-    for(int i = 0 ; i< 47 ; i++){
-         for(int j = 0 ; j< 47 ; j++){
+    for(int i = 0 ; i< NUM_STATIONS ; i++){
+         for(int j = 0 ; j< NUM_STATIONS ; j++){
             System.out.print(TIMES_MATRIX[i][j]+"\t");
             
          }
          System.out.println();
       }  
     System.out.println("PATH_MATRIX");  
-    for(int i = 0 ; i< 47 ; i++){
-         for(int j = 0 ; j< 47 ; j++){
+    for(int i = 0 ; i< NUM_STATIONS ; i++){
+         for(int j = 0 ; j< NUM_STATIONS ; j++){
             System.out.print(PATH_MATRIX[i][j]+"\t");
             
          }

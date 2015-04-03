8cf3777a93721e3c7992c59a1bb8434fb13407bf
ciceropc <ciceropcosta@gmail.com>
2015-04-03 15:12:40 -0700

    Logo and Splashscreen

--- src/com/example/skytrainvancouver/SplashActivity.java
+++ src/com/example/skytrainvancouver/SplashActivity.java
@@ -10,7 +10,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class SplashActivity extends Activity {
-	static int TIME_TASK = 3000; // each 1000 == 1s
+	static int TIME_TASK = 4000; // each 1000 == 1s
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
